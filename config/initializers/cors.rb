9caf3be6ef31fa5370e029278c3b2da0bae50cd9
Sandy <sedwardscode@gmail.com>
2018-09-16 16:59:31 -0400

    altered cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3001'
+    origins 'https://vibelist-client.herokuapp.com/'
 
     resource '*',
       headers: :any,
