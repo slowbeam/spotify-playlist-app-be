6341447b5eef365f6789e9c5f6e6b546fc63d84b
Sandy <sedwardscode@gmail.com>
2018-09-16 13:28:20 -0400

    minor

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ENV['SECRET'] = 'p4nd4_f0r_S4l3'
+
+ENV['CLIENT_ID'] = 'd9dbfa9ebabf431081a7d8c7df553196'
+
+ENV['CLIENT_SECRET'] = 'b1744cbd42244ac681a388e02371ed63'
