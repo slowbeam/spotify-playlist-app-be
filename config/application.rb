40821852bff24807d382ea2251cccc090c5be43c
Sandy <sedwardscode@gmail.com>
2018-09-17 12:04:06 -0400

    switched to specific cors url

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module SpotifyPlaylistAppBe
 
     config.middleware.insert_before 0, Rack::Cors do
      allow do
-       origins '*'
+       origins 'http://vibelist.co'
        resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
