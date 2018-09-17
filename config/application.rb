95c1fbeb59fd2c88ee9304db3ce478e496915bf4
Sandy <sedwardscode@gmail.com>
2018-09-17 11:43:06 -0400

    minor

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,7 @@ module SpotifyPlaylistAppBe
     config.middleware.insert_before 0, "Rack::Cors" do
      allow do
        origins '*'
-       resource '*', headers: :any, methods: [:get, :post, :patch, :options]
+       resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
 
