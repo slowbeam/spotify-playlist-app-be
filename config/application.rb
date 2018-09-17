64684093f410d8372b49030df1e4f414f855123f
Sandy <sedwardscode@gmail.com>
2018-09-16 21:56:33 -0400

    added www to all routes

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module SpotifyPlaylistAppBe
 
     config.middleware.insert_before 0, Rack::Cors do
      allow do
-       origins 'http://vibelist.co/'
+       origins 'http://www.vibelist.co/'
        resource '*', headers: :any, methods: [:get, :post, :options]
      end
    end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://vibelist.co/'
+    origins 'http://www.vibelist.co/'
 
     resource '*',
       headers: :any,
